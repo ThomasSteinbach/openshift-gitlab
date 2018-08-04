4a4c29e3f5e441ec8b785ffb6852e46606d8bca9
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-08-05 01:29:06 +0200

    added privileged user

--- setup-project.sh
+++ setup-project.sh
@@ -1,5 +1,7 @@
 oc new-project gitlab || oc project gitlab
 sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n gitlab
+sudo oc create sa privilegeduser -n gitlab
+sudo oc adm policy add-scc-to-user privileged -z privilegedusera -n gitlab
 oc new-app -f secret-template.yaml
 oc-apply
