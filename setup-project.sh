63842f6e8b55ee62e166fc1456e8512e956257a2
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-08-09 10:57:07 +0200

    fixed typo in setup-project.sh

--- setup-project.sh
+++ setup-project.sh
@@ -2,6 +2,6 @@ oc new-project gitlab || oc project gitlab
 sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n gitlab
 sudo oc create sa privilegeduser -n gitlab
-sudo oc adm policy add-scc-to-user privileged -z privilegedusera -n gitlab
+sudo oc adm policy add-scc-to-user privileged -z privilegeduser -n gitlab
 oc new-app -f secret-template.yaml
 oc-apply
